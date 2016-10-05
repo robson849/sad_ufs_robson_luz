d72939fe6a3140133fff5bf486f812a9a899b5a3
Robson <robson849@gmail.com>
2016-10-05 10:46:10 -0300

    atividade 6

---
+++ atividade6/atividade6.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sad.atividade6
+(
+  id_cliente DOUBLE PRECISION
+, nome TEXT
+, telefone TEXT
+, email TEXT
+, cpf TEXT
+, id_endereco DOUBLE PRECISION
+, cidade TEXT
+, estado TEXT
+, logradouro TEXT
+, id_usuario DOUBLE PRECISION
+, "login" TEXT
+, senha TEXT
+)
+;
+
+INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES (null,null,null,null,null, 1,0,E'Aracaju',E'Sergipe',E'Avenida A',null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES (null,null,null,null,null, 3,0,E'Salvador',E'Bahia',E'Rua 205',null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES (null,null,null,null,null, 5,0,E'Belo Horizonte',E'Minas Gerais',E'Rua X',null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES ( 1,0,E'Administrador',E'9999-9999',null,null,null,null,null,null,null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES ( 2,0,E'Robson',E'9389-9382',E'robson@gmail.com',E' 87627488529,0',null,null,null,null,null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES ( 3,0,E'Caio ',E'9941-4959',E'caio24@hotmail.com',E' 21455443423,0',null,null,null,null,null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES ( 4,0,E'Pedro',E'8172-3382',E'pedro123@yahoo.com',E' 12566355345,0',null,null,null,null,null,null,null);INSERT INTO sad.atividade6(id_cliente, nome, telefone, email, cpf, id_endereco, cidade, estado, logradouro, id_usuario, "login", senha) VALUES ( 5,0,E'Gabriel',E'9892-0981',E'gabriel653@gmail.com',E' 66644334423,0',null,null,null,null,null,null,null);
